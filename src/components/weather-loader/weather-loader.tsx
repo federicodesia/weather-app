b5e6f1475bb5e609d46f406a285fde3b3bed1dba
Federico De Sía <desiafederico@gmail.com>
2022-06-27 04:35:57 -0300

    WeatherLoader with Icons

--- src/components/weather-loader/weather-loader.tsx
+++ src/components/weather-loader/weather-loader.tsx
@@ -1,12 +1,11 @@
+import { BsCloudyFill } from 'react-icons/bs';
 import styles from './weather-loader.module.css';
 
 function WeatherLoader() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.cloud}></div>
-            <div className={styles.cloud}></div>
-        </div>
-    )
+    return <div className={styles.container}>
+        <BsCloudyFill className={styles.cloud} />
+        <BsCloudyFill className={styles.cloud} />
+    </div>
 }
 
 export default WeatherLoader;
